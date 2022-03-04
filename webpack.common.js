0a24e4dbda6698ff8436929941246b5c7c5937a2
John Stark <jhnstrk@gmail.com>
2022-03-04 21:54:20 +0100

    Vue loader plugin changed
    https://github.com/vuejs/vue-loader/issues/1688

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   entry: './src/index.ts',
